14a310881c0e723d0a8adf8032d6344d979f05cd
Rish46h <tyagirishabh121@gmail.com>
2023-12-30 15:49:51 +0530

    On branch main  Your branch is up to date with 'origin/main'.  Changes to be committed: 	modified:   src/Context/ShopContext.jsx 	modified:   src/Pages/ShopCategory.jsx

--- src/Context/ShopContext.jsx
+++ src/Context/ShopContext.jsx
@@ -17,13 +17,13 @@ const ShopContextProvider = (props) => {
   const [cartItems, setCartItems] = useState(getDefaultCart());
 
   useEffect(() => {
-    fetch('http://localhost:4000/allproducts') 
+    fetch('https://bassback.onrender.com/allproducts') 
           .then((res) => res.json()) 
           .then((data) => setProducts(data))
 
     if(localStorage.getItem("auth-token"))
     {
-      fetch('http://localhost:4000/getcart', {
+      fetch('https://bassback.onrender.com/getcart', {
       method: 'POST',
       headers: {
         Accept:'application/form-data',
@@ -63,7 +63,7 @@ const ShopContextProvider = (props) => {
     setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] + 1 }));
     if(localStorage.getItem("auth-token"))
     {
-      fetch('http://localhost:4000/addtocart', {
+      fetch('https://bassback.onrender.com/addtocart', {
       method: 'POST',
       headers: {
         Accept:'application/form-data',
@@ -81,7 +81,7 @@ const ShopContextProvider = (props) => {
     setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] - 1 }));
     if(localStorage.getItem("auth-token"))
     {
-      fetch('http://localhost:4000/removefromcart', {
+      fetch('https://bassback.onrender.com/removefromcart', {
       method: 'POST',
       headers: {
         Accept:'application/form-data',
