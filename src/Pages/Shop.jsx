8f3a1e29b02f12b82fa88a5bfae44f50ddfeb73f
Rish46h <tyagirishabh121@gmail.com>
2023-12-30 15:43:27 +0530

    On branch main  Your branch is up to date with 'origin/main'.  Changes to be committed: 	modified:   src/Pages/LoginSignup.jsx 	modified:   src/Pages/Shop.jsx

--- src/Pages/Shop.jsx
+++ src/Pages/Shop.jsx
@@ -11,10 +11,10 @@ const Shop = () => {
   const [newcollection, setNewCollection] = useState([]);
 
   const fetchInfo = () => { 
-    fetch('http://localhost:4000/popularinwomen') 
+    fetch('https://bassback.onrender.com/popularinwomen') 
             .then((res) => res.json()) 
             .then((data) => setPopular(data))
-    fetch('http://localhost:4000/newcollections') 
+    fetch('https://bassback.onrender.com/newcollections') 
             .then((res) => res.json()) 
             .then((data) => setNewCollection(data))
     }
