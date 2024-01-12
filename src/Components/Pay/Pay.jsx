eb0d93930b45e7b54f3b58dea070c30a764339c6
Rish46h <tyagirishabh121@gmail.com>
2024-01-12 15:28:49 +0530

    On branch main  Your branch is up to date with 'origin/main'.  Changes to be committed: 	modified:   src/Components/Pay/Pay.jsx

--- src/Components/Pay/Pay.jsx
+++ src/Components/Pay/Pay.jsx
@@ -6,7 +6,7 @@ const Pay = () => {
   const deliver = alert("Your Order will be deliverd in 3 to 5 working days");
   return (
     <div className='pay'>
-            <h2>OR By QR</h2>
+            
             <div className="qr">
             <img src={qr} alt="" />
             </div>
